d1cf1685c0890196c8e640db2ca6d07c7babc393
AlexeyYakovec <yakovec8952@gmail.com>
2024-07-24 08:44:07 +0500

    Step 3: Create header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Montserrat } from "next/font/google";
 import "./globals.css";
+import { Header } from "@/components/shared/header";
 
 const montserrat = Montserrat({
    subsets: ["latin"],
@@ -21,6 +22,7 @@ export default function RootLayout({
    return (
       <html lang="en">
          <body className={montserrat.className}>
+            <Header />
             <main className="min-h-screen">{children}</main>
          </body>
       </html>
